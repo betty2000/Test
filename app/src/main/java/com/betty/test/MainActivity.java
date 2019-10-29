14262694ebf13acba3d174df037cb74b8be06d71
miia <miia@com.tw>
2019-10-29 16:29:36 +0800

    first

---
+++ app/src/main/java/com/betty/test/MainActivity.java
@@ -0,0 +1,91 @@
+package com.betty.test;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    public static  final String TAG = MainActivity.class.getSimpleName();
+    int secret = new Random().nextInt(10)+1;
+    int counter = 0;
+    private EditText ednumber;
+    private TextView edcounter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Log.d(TAG,"secret:"+secret);
+        ednumber =findViewById(R.id.ednumber);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    public void onClick(View view) {
+        int n = Integer.parseInt(ednumber.getText().toString());
+        if (n > secret) {
+           new AlertDialog.Builder(MainActivity.this)
+                   .setTitle("hahaa")
+                   .setMessage("Smaller")
+                   .setPositiveButton("OK",null )
+                   .show();
+
+        } else if (n < secret) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("hahaa")
+                    .setMessage("Bigger")
+                    .setPositiveButton("OK",null )
+                    .show();
+        } else {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("hahaa")
+                    .setMessage("Bingo")
+                    .setPositiveButton("OK",null )
+                    .show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
